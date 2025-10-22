091fd13290a7cff262ab001d9b34676929a76e3c
unknown <jvrdf@cesar.school>
2025-10-22 17:48:35 -0300

    superusuario atualizado

--- fdsprojeto/manage.py
+++ fdsprojeto/manage.py
@@ -4,6 +4,14 @@ import os
 import sys
 
 
+if os.environ.get("RENDER") == "true":
+    import django
+    django.setup()
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin123")
+        print("✅ Superusuário 'admin' criado automaticamente.")
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fdsprojeto.settings')
