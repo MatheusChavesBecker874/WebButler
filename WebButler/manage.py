96e9d65e0b8ba1897fdca621324ef3adb8cf752f
Hirus <146269273+Hirouzs@users.noreply.github.com>
2025-10-22 17:45:51 -0300

    Update manage.py
    adiconando criação superusuario

--- WebButler/manage.py
+++ WebButler/manage.py
@@ -15,8 +15,17 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+        if os.environ.get("RENDER") == "true":
+    import django
+    django.setup()
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin123")
+        print("✅ Superusuário 'admin' criado automaticamente.")
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
     main()
+
