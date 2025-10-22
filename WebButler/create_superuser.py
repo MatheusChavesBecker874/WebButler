7f57e18fe77afa43636b819b8d6e1f0b592037ee
Hirus <146269273+Hirouzs@users.noreply.github.com>
2025-10-22 17:38:20 -0300

    Update create_superuser.py

--- WebButler/create_superuser.py
+++ WebButler/create_superuser.py
@@ -1,19 +1,1 @@
-import os
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fdsprojeto.settings')
-django.setup()
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-username = "admin"
-email = "admin@admin.com"
-password = "senha123"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superusuário criado com sucesso!")
-else:
-    print("Superusuário já existe.")
