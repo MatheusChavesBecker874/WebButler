818ac6af64f232e4525027036023eb5be49fa6c7
MatheusChavesBecker <135650866+MatheusChavesBecker@users.noreply.github.com>
2025-10-25 19:29:50 -0300

    Corrige erro de falta de virgula na migration inicial

--- WebButler/projeto/migrations/0001_initial.py
+++ WebButler/projeto/migrations/0001_initial.py
@@ -17,8 +17,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('nome', models.CharField(max_length=100)),
-            ]
-        )
+            ],
+        ),
         
         migrations.CreateModel(
             name='Rotina',
