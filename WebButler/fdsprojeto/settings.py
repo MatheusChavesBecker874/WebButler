0599580aa27de5760cc4c18da5d62905b5851a9c
MatheusChavesBecker <135650866+MatheusChavesBecker@users.noreply.github.com>
2025-11-10 18:34:32 -0300

    mudança no DIRS

--- WebButler/fdsprojeto/settings.py
+++ WebButler/fdsprojeto/settings.py
@@ -37,7 +37,7 @@ ROOT_URLCONF = 'fdsprojeto.urls'
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "projeto", "templates")],
+        "DIRS": [BASE_DIR / "projeto" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
